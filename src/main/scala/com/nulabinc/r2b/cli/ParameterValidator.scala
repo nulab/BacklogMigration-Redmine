38c119c15be63b3a30636f92b639eee9753b9a98
uchidayuichi <uchida@nulab-inc.com>
2017-04-22 14:01:16 +0900

    Add checking admin auth.

--- src/main/scala/com/nulabinc/r2b/cli/ParameterValidator.scala
+++ src/main/scala/com/nulabinc/r2b/cli/ParameterValidator.scala
@@ -1,7 +1,7 @@
 package com.nulabinc.r2b.cli
 
 import com.nulabinc.backlog.migration.modules.{ServiceInjector => BacklogInjector}
-import com.nulabinc.backlog.migration.service.{UserService => BacklogUserService}
+import com.nulabinc.backlog.migration.service.{SpaceService}
 import com.nulabinc.backlog.migration.utils.{ConsoleOut, Logging}
 import com.nulabinc.backlog4j.BacklogAPIException
 import com.nulabinc.r2b.conf.AppConfiguration
@@ -17,68 +17,73 @@ import com.taskadapter.redmineapi.{RedmineAuthenticationException, RedmineTransp
 class ParameterValidator(config: AppConfiguration) extends Logging {
 
   def validate(): Seq[String] = {
-    val redmineErrors: Seq[String] = validateConfigRedmine()
-    val backlogErrors: Seq[String] = validateConfigBacklog()
+    val validateRedmine = validateConfigRedmine()
+    val validateBacklog = validateConfigBacklog()
     ConsoleOut.info(Messages("cli.param.get.project", Messages("common.redmine")))
     val optRedmineProject = optProject()
-    if (config.importOnly) backlogErrors
-    else {
-      if (redmineErrors.nonEmpty) {
-        backlogErrors union redmineErrors
-      } else {
-        backlogErrors union
-          redmineErrors union
-          validateProject(optRedmineProject) union
-          validProjectKey(config.backlogConfig.projectKey)
-      }
+
+    val messages = Seq(validateBacklog, validProjectKey(config.backlogConfig.projectKey), validateAuthBacklog(validateBacklog)).flatten
+
+    if (config.importOnly) {
+      messages
+    } else {
+      messages union Seq(validateRedmine, validateProject(optRedmineProject)).flatten
     }
   }
 
-  private[this] def validateProject(optRedmineProject: Option[Project]): Seq[String] = {
+  private[this] def validateProject(optRedmineProject: Option[Project]): Option[String] = {
     optRedmineProject match {
-      case None => Seq(s"- ${Messages("cli.param.error.disable.project", config.redmineConfig.projectKey)}")
-      case _    => Seq.empty[String]
+      case None => Some(s"- ${Messages("cli.param.error.disable.project", config.redmineConfig.projectKey)}")
+      case _    => None
     }
   }
 
-  private[this] def validateConfigBacklog(): Seq[String] = {
+  private[this] def validateConfigBacklog(): Option[String] = {
     ConsoleOut.info(Messages("cli.param.check.access", Messages("common.backlog")))
     val messages = try {
-      val injector    = BacklogInjector.createInjector(config.backlogConfig)
-      val userService = injector.getInstance(classOf[BacklogUserService])
-      userService.allUsers()
-      Seq.empty[String]
+      val injector     = BacklogInjector.createInjector(config.backlogConfig)
+      val spaceService = injector.getInstance(classOf[SpaceService])
+      spaceService.space()
+      None
     } catch {
       case unknown: BacklogAPIException if unknown.getStatusCode == 404 =>
-        Seq(s"- ${Messages("cli.param.error.disable.host", Messages("common.backlog"), config.backlogConfig.url)}")
+        Some(s"- ${Messages("cli.param.error.disable.host", Messages("common.backlog"), config.backlogConfig.url)}")
       case _: Throwable =>
-        Seq(s"- ${Messages("cli.param.error.disable.access", Messages("common.backlog"))}")
+        Some(s"- ${Messages("cli.param.error.disable.access", Messages("common.backlog"))}")
     }
     messages
   }
 
-  private[this] def validateConfigRedmine(): Seq[String] = {
+  private[this] def validateAuthBacklog(resultValidateConfig: Option[String]): Option[String] = {
+    if (resultValidateConfig.isEmpty) {
+      ConsoleOut.info(Messages("cli.param.check.admin"))
+      val injector     = BacklogInjector.createInjector(config.backlogConfig)
+      val spaceService = injector.getInstance(classOf[SpaceService])
+      if (spaceService.hasAdmin()) None
+      else Some(s"- ${Messages("cli.param.error.auth.backlog")}")
+    } else None
+  }
+
+  private[this] def validateConfigRedmine(): Option[String] = {
     ConsoleOut.info(Messages("cli.param.check.access", Messages("common.redmine")))
     try {
       val injector    = RedmineInjector.createInjector(config.redmineConfig)
       val userService = injector.getInstance(classOf[RedmineUserService])
       userService.allUsers()
-      Seq.empty[String]
+      None
     } catch {
       case _: RedmineAuthenticationException =>
-        Seq("- " + Messages("cli.param.error.auth", Messages("common.redmine")))
+        Some(s"- ${Messages("cli.param.error.auth", Messages("common.redmine"))}")
       case _: RedmineTransportException =>
-        Seq("- " + Messages("cli.param.error.disable.host", Messages("common.redmine"), config.redmineConfig.url))
+        Some(s"- ${Messages("cli.param.error.disable.host", Messages("common.redmine"), config.redmineConfig.url)}")
       case _: Throwable =>
-        Seq("- " + Messages("cli.param.error.disable.access", Messages("common.redmine")))
+        Some(s"- ${Messages("cli.param.error.disable.access", Messages("common.redmine"))}")
     }
   }
 
-  private[this] def validProjectKey(projectKey: String): Seq[String] = {
-    if (projectKey.matches("""^[0-9A-Z_]+$"""))
-      Seq.empty[String]
-    else
-      Seq(s"- ${Messages("cli.param.error.project_key", projectKey)}")
+  private[this] def validProjectKey(projectKey: String): Option[String] = {
+    if (projectKey.matches("""^[0-9A-Z_]+$""")) None
+    else Some(s"- ${Messages("cli.param.error.project_key", projectKey)}")
   }
 
   private[this] def optProject(): Option[Project] = {
