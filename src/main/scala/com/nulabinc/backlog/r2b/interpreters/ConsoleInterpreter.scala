b2ced77533d1f0ca151c08549b21bf589d3f316a
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2018-03-20 13:49:59 +0900

    Add: read command

--- src/main/scala/com/nulabinc/backlog/r2b/interpreters/ConsoleInterpreter.scala
+++ src/main/scala/com/nulabinc/backlog/r2b/interpreters/ConsoleInterpreter.scala
@@ -2,13 +2,14 @@ package com.nulabinc.backlog.r2b.interpreters
 
 import cats.free.Free
 import cats.~>
-import com.nulabinc.backlog.migration.common.utils.ConsoleOut
+import com.nulabinc.backlog.migration.common.utils.{ConsoleOut, Logging}
+import com.osinka.i18n.Messages
 
 import scala.concurrent.Future
 
 sealed trait ConsoleADT[A]
 case class Print(str: String) extends ConsoleADT[Unit]
-//case class Read(prompt: String) extends ConsoleADT[String]
+case class Read() extends ConsoleADT[String]
 
 object ConsoleDSL {
 
@@ -17,14 +18,20 @@ object ConsoleDSL {
   def print(str: String): ConsoleProgram[Unit] =
     Free.liftF(Print(str))
 
+  def read(): ConsoleProgram[String] =
+    Free.liftF(Read())
+
 }
 
-class ConsoleInterpreter extends (ConsoleADT ~> Future) {
+class ConsoleInterpreter extends (ConsoleADT ~> Future) with Logging {
 
   def apply[A](fa: ConsoleADT[A]): Future[A] = fa match  {
     case Print(str) => Future.successful {
       ConsoleOut.println(str)
       ()
+    }
+    case Read() => Future.successful {
+      scala.io.StdIn.readLine(Messages("cli.confirm"))
     }
   }
 }
