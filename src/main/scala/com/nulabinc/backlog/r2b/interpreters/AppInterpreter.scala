fdecbe4053504568f21217eb775b6212f78d71df
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2018-03-20 15:18:24 +0900

    Update: supports streaming

--- src/main/scala/com/nulabinc/backlog/r2b/interpreters/AppInterpreter.scala
+++ src/main/scala/com/nulabinc/backlog/r2b/interpreters/AppInterpreter.scala
@@ -1,20 +1,23 @@
 package com.nulabinc.backlog.r2b.interpreters
 
-import backlog4s.dsl.ApiDsl.ApiPrg
+import backlog4s.dsl.ApiDsl.{ApiADT, ApiPrg}
 import backlog4s.interpreters.AkkaHttpInterpret
 import backlog4s.streaming.ApiStream.ApiStream
 import cats.free.Free
-import cats.{Monad, ~>}
+import cats.~>
 import cats.implicits._
 import com.nulabinc.backlog.r2b.interpreters.AppDSL.AppProgram
 import com.nulabinc.backlog.r2b.interpreters.ConsoleDSL.ConsoleProgram
+import monix.reactive.Observable
+import org.reactivestreams.{Publisher, Subscriber}
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
-sealed trait AppADT[A]
+sealed trait AppADT[+A]
 case class Pure[A](a: A) extends AppADT[A]
 case class Backlog[A](prg: ApiPrg[A]) extends AppADT[A]
-case class BacklogStream[A](prg: ApiStream[A]) extends AppADT[A]
+case class BacklogStream[A](prg: ApiStream[A]) extends AppADT[Observable[Seq[A]]]
 case class Console[A](prg: ConsoleProgram[A]) extends AppADT[A]
 
 object AppDSL {
@@ -27,8 +30,8 @@ object AppDSL {
   def backlog[A](prg: ApiPrg[A]): AppProgram[A] =
     Free.liftF(Backlog(prg))
 
-  def backlogStream[A](prg: ApiStream[A]): AppProgram[A] =
-    Free.liftF(BacklogStream(prg))
+  def backlogStream[A](prg: ApiStream[A]): AppProgram[Observable[Seq[A]]] =
+    Free.liftF[AppADT, Observable[Seq[A]]](BacklogStream(prg))
 
   def console[A](prg: ConsoleProgram[A]): AppProgram[A] =
     Free.liftF(Console(prg))
@@ -39,15 +42,28 @@ case class AppInterpreter(backlogInterpreter: AkkaHttpInterpret,
                           consoleInterpreter: ConsoleInterpreter)
                          (implicit ec: ExecutionContext) extends (AppADT ~> Future) {
 
-  implicit val monad = implicitly[Monad[Future]]
-
   def run[A](prg: AppProgram[A]): Future[A] =
     prg.foldMap(this)
 
   override def apply[A](fa: AppADT[A]): Future[A] = fa match {
     case Pure(a) => Future.successful(a)
     case Backlog(prg) => backlogInterpreter.run(prg)
-//    case BacklogStream(prg) => backlogInterpreter.runStream(prg)
+    case BacklogStream(stream) => Future.successful {
+      Observable.fromReactivePublisher[Seq[A]](
+        (s: Subscriber[_ >: Seq[A]]) => {
+          backlogInterpreter.runStream(
+            stream.map { value =>
+              val a = value.asInstanceOf[Seq[A]]
+              s.onNext(a) // publish data
+              Seq(a)
+            }
+          ).onComplete {
+            case Success(_) => s.onComplete()
+            case Failure(ex) => s.onError(ex)
+          }
+        }
+      )
+    }
     case Console(prg) => prg.foldMap(consoleInterpreter)
   }
 }
