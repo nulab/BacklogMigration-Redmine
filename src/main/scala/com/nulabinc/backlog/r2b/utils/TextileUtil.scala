b75a235bc12ba7dcfda58fbcbb899ea45575f16d
Yuichi Uchida <yuichi0301@gmail.com>
2018-10-17 14:06:20 +0900

    Bug fix when value is null.

--- src/main/scala/com/nulabinc/backlog/r2b/utils/TextileUtil.scala
+++ src/main/scala/com/nulabinc/backlog/r2b/utils/TextileUtil.scala
@@ -4,11 +4,13 @@ import com.nulabinc.backlog.migration.common.domain.BacklogTextFormattingRule
 object TextileUtil {
 
   def convert(value: String, backlogTextFormattingRule: BacklogTextFormattingRule) = {
-    if (backlogTextFormattingRule.value == "backlog") {
-      value.replace("<pre>", "{code}").replace("</pre>", "{/code}")
-    } else {
-      value.replace("<pre>", "```").replace("</pre>", "```")
-    }
+    if (Option(value).isDefined) {
+      if (backlogTextFormattingRule.value == "backlog") {
+        value.replace("<pre>", "{code}").replace("</pre>", "{/code}")
+      } else {
+        value.replace("<pre>", "```").replace("</pre>", "```")
+      }
+    } else value
   }
 
 }
