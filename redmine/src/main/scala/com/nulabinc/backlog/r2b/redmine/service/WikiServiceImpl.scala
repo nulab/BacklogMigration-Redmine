c7ed1aaa46551d694c01128f5bf8d5b8ef240d75
Shoma Nishitateno <shoma416@gmail.com>
2018-10-19 16:44:55 +0900

    update: handle format exception

--- redmine/src/main/scala/com/nulabinc/backlog/r2b/redmine/service/WikiServiceImpl.scala
+++ redmine/src/main/scala/com/nulabinc/backlog/r2b/redmine/service/WikiServiceImpl.scala
@@ -1,12 +1,15 @@
 package com.nulabinc.backlog.r2b.redmine.service
 
+import java.net.URLEncoder
+
 import javax.inject.Inject
 import com.nulabinc.backlog.migration.common.utils.Logging
 import com.nulabinc.backlog.r2b.redmine.conf.RedmineApiConfiguration
-import com.taskadapter.redmineapi.{NotFoundException, RedmineInternalError, RedmineManager}
+import com.taskadapter.redmineapi.{NotFoundException, RedmineFormatException, RedmineInternalError, RedmineManager}
 import com.taskadapter.redmineapi.bean.{WikiPage, WikiPageDetail}
 
 import scala.collection.JavaConverters._
+import scala.util.Try
 
 /**
   * @author uchida
@@ -34,8 +37,23 @@ class WikiServiceImpl @Inject()(apiConfig: RedmineApiConfiguration, redmine: Red
       case e: NotFoundException =>
         logger.warn(s"Failed to get wiki details. NotFoundException: ${e.getMessage} Title: $pageTitle")
         None
+      case e: RedmineFormatException =>
+        val url = s"${apiConfig.url}/projects/${apiConfig.projectKey}/wiki/${encode(pageTitle)}.json?include=attachments&key=${apiConfig.key}"
+
+        Try {
+          scala.io.Source.fromURL(url, "UTF-8").mkString
+        }.recover {
+          case e: Throwable =>
+            e.getMessage
+        }.map { res =>
+          logger.warn(s"Failed to get wiki details. RedmineFormatException: ${e.getMessage} Title: $pageTitle Raw response: $res")
+        }
+        None
       case e: Throwable =>
         throw e
     }
   }
+
+  private def encode(str: String): String =
+    URLEncoder.encode(str, "UTF-8")
 }
