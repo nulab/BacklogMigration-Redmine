0c04a7d90d384869e9e393360725ddf3214eaebe
Shoma Nishitateno <shoma.nishitateno@nulab.com>
2020-09-17 17:11:45 +0900

    fix: test

--- src/test/scala/integration/helper/SimpleFixture.scala
+++ src/test/scala/integration/helper/SimpleFixture.scala
@@ -1,23 +1,29 @@
 package integration.helper
 
-import java.io.FileInputStream
+import java.io.{File, FileInputStream}
 import java.text.SimpleDateFormat
 import java.util.{Date, Locale, Properties}
 
-import better.files.File
 import com.nulabinc.backlog.migration.common.conf.{BacklogApiConfiguration, ExcludeOption}
+import com.nulabinc.backlog.migration.common.dsl.{ConsoleDSL, StorageDSL}
+import com.nulabinc.backlog.migration.common.interpreters.{JansiConsoleDSL, LocalStorageDSL}
+import com.nulabinc.backlog.migration.common.services.StatusMappingFileService
 import com.nulabinc.backlog.migration.common.utils.IOUtil
 import com.nulabinc.backlog.r2b.conf.AppConfiguration
+import com.nulabinc.backlog.r2b.domain.mappings.RedmineStatusMappingItem
 import com.nulabinc.backlog.r2b.mapping.core._
+import com.nulabinc.backlog.r2b.mapping.domain.MappingJsonProtocol._
 import com.nulabinc.backlog.r2b.mapping.domain.{Mapping, MappingsWrapper}
 import com.nulabinc.backlog.r2b.mapping.service.{MappingPriorityServiceImpl, MappingUserServiceImpl}
 import com.nulabinc.backlog.r2b.redmine.conf.RedmineApiConfiguration
 import com.nulabinc.backlog4j.conf.{BacklogConfigure, BacklogPackageConfigure}
 import com.nulabinc.backlog4j.{BacklogClient, BacklogClientFactory}
 import com.osinka.i18n.Lang
-import com.taskadapter.redmineapi.RedmineManagerFactory
+import com.taskadapter.redmineapi.bean.Project
+import com.taskadapter.redmineapi.{RedmineManager, RedmineManagerFactory}
+import monix.eval.Task
+import monix.execution.Scheduler
 import spray.json.{JsNumber, JsonParser}
-import com.nulabinc.backlog.r2b.mapping.domain.MappingJsonProtocol._
 
 import scala.jdk.CollectionConverters._
 
@@ -28,58 +34,71 @@ trait SimpleFixture {
 
   implicit val userLang = if (Locale.getDefault.equals(Locale.JAPAN)) Lang("ja") else Lang("en")
 
+  import com.nulabinc.backlog.r2b.deserializers.RedmineMappingDeserializer._
+
+  private implicit val exc: Scheduler               = monix.execution.Scheduler.Implicits.global
+  private implicit val storageDSL: StorageDSL[Task] = LocalStorageDSL()
+  private implicit val consoleDSL: ConsoleDSL[Task] = JansiConsoleDSL()
+
   val dateFormat      = new SimpleDateFormat("yyyy-MM-dd")
   val timestampFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
 
   val optAppConfiguration    = getAppConfiguration
   val mappingUserService     = new MappingUserServiceImpl(unmarshal(MappingDirectory.USER_MAPPING_FILE))
   val mappingPriorityService = new MappingPriorityServiceImpl(unmarshal(MappingDirectory.PRIORITY_MAPPING_FILE))
-  val mappingStatusService   = new MappingStatusServiceImpl(unmarshal(MappingDirectory.STATUS_MAPPING_FILE))
 
-  def redmine() = {
+  def redmine: RedmineManager =
     optAppConfiguration match {
       case Some(appConfiguration) => RedmineManagerFactory.createWithApiKey(appConfiguration.redmineConfig.url, appConfiguration.redmineConfig.key)
-      case _                      => throw new RuntimeException()
+      case _                      => throw new RuntimeException("App configuration is missing")
     }
-  }
 
-  def backlog() = {
+  def backlog: BacklogClient =
     optAppConfiguration match {
       case Some(appConfiguration) => getBacklogClient(appConfiguration.backlogConfig)
-      case _                      => throw new RuntimeException()
+      case _                      => throw new RuntimeException("App configuration is missing")
     }
-  }
 
-  def redmineProject() = {
+  def redmineProject: Project =
     optAppConfiguration match {
-      case Some(appConfiguration) => redmine().getProjectManager.getProjectByKey(appConfiguration.redmineConfig.projectKey)
+      case Some(appConfiguration) => redmine.getProjectManager.getProjectByKey(appConfiguration.redmineConfig.projectKey)
       case _                      => throw new RuntimeException()
     }
-  }
 
   def convertUser(target: String): String = {
     mappingUserService.convert(target)
   }
 
-  def convertStatus(target: String): String = {
-    mappingStatusService.convert(target)
-  }
+  def convertStatus(target: String): String =
+    StatusMappingFileService
+      .getMappings[RedmineStatusMappingItem, Task](
+        path = new File(
+          MappingDirectory.STATUS_MAPPING_FILE
+        ).getAbsoluteFile.toPath
+      )
+      .runSyncUnsafe()
+      .getOrElse(throw new RuntimeException("cannot convert status. target: " + target))
+      .find(_.src.value == target)
+      .getOrElse(throw new RuntimeException("cannot resolve status. target: " + target))
+      .optDst
+      .map(_.value)
+      .getOrElse("")
 
   def convertPriority(target: String): String = {
     mappingPriorityService.convert(target)
   }
 
   private[this] def unmarshal(strPath: String): Seq[Mapping] = {
-    val path = File(strPath).path.toAbsolutePath
+    val path = new File(strPath).getAbsoluteFile.toPath
     val json = IOUtil.input(path).getOrElse("")
     JsonParser(json).convertTo[MappingsWrapper].mappings
   }
 
   private[this] def getAppConfiguration: Option[AppConfiguration] = {
-    val file = File("app.properties")
+    val file = new File("app.properties")
     if (file.exists()) {
       val prop: Properties = new Properties()
-      prop.load(new FileInputStream(file.path.toFile))
+      prop.load(new FileInputStream(file.getAbsoluteFile))
       val redmineKey: String = prop.getProperty("redmine.key")
       val redmineUrl: String = prop.getProperty("redmine.url")
       val backlogKey: String = prop.getProperty("backlog.key")
